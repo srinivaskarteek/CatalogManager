852be6c3a20c31add04fb485f35dcdeed449365b
IDEV\SESA477631 <srinivasa.karteek@non.schneider-electric.com>
2019-02-06 14:27:22 +0530

    Fixing the Routing

--- client/CatalogManager/src/app/app-routing.module.ts
+++ client/CatalogManager/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PackageDetailsComponent } from './packagedetails/packagedetails.component';
+import { PackagelistComponent } from './packagelist/packagelistcomponent';
 
 const routes: Routes = [
+  { path: '',  redirectTo: 'packagedetails', pathMatch: 'full' },
+  { path: 'packagedetails',  component: PackagelistComponent },
   { path: 'viewdetails',  component: PackageDetailsComponent }
 ];
 
